0c16ea5a54aae9b2668cef620651fb818b569bd9
WendyG07 <wendygermn@gmail.com>
2020-01-29 14:41:01 -0500

    Commit mejora

--- hbs/helpers.js
+++ hbs/helpers.js
@@ -1,10 +1,12 @@
 const hbs = require('hbs');
 
 // Helpers
+//hbs para renderizar el index
+//este método es para que el año se actualice autmáticamente en la página
 hbs.registerHelper('getAnio', () => {
     return new Date().getFullYear();
 });
-
+//este método es para poner en letras mayúsculas el nombre
 hbs.registerHelper('capitalizar', (texto) => {
     let palabras = texto.split(' ');
     palabras.forEach((palabra, idx) => {
